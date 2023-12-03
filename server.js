38eb4604ce03c8ebfe3e8ceee22e572f4a64cb6d
vivian <41368076+veevyo@users.noreply.github.com>
2023-12-02 20:02:41 -0600

    made a shop page plus did some node.js stuff :3

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 const express = require('express')
 const app = express()
+const path = require("path");
 const port = 3000
 
 app.use(express.static('public'))
-app.use('/CSS', express.static(__dirname + '/public/CSS'))
-app.use('/images', express.static(__dirname + '/public/images'))
-// app.use('/views', express.static(__dirname + 'public/views'))
+//app.use('/CSS', express.static(__dirname + '/public/CSS'))
+app.use('/media', express.static(__dirname + '/public/media'))
+app.use('/views', express.static(__dirname + '/views'))
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('LandingPage.ejs')
+    res.render('index.ejs')
 });
 
-// app.get('/about', (req, res) => {
-//     res.render('views/LandingPage.ejs')
-// });
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+});
+
+//temporary. currently anyone is allowed to access the shop at /shop.
+//intended for now. should be fixed l8r.
+app.get('/shop', (req, res) => {
+    res.render('shop.ejs')
+})
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`)
